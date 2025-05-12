8530cceb7c0dbdd8b10aea9410cf05886cf747f0
AkshayPalSingh <akshaypalsingh523@gmail.com>
2025-05-12 13:04:06 +0530

    Time: 0 ms (100%), Space: 40.3 MB (64.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,16 @@
-
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return fib(n - 1) + fib(n - 2);
-        }
+        int[] memo = new int[n + 1];
+        return helper(n, memo);
+    }
+
+    private int helper(int n, int[] memo) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        if (memo[n] != 0) return memo[n];
+
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
+        return memo[n];
     }
 }
